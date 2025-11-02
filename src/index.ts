4d98f268ce526b50b2c042d55f9ffe9017d8116f
horstrs <horst.marcelo@gmail.com>
2025-11-02 10:20:15 -0300

    Added health check and redirected / to /app/

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
-import express, { response } from "express";
-import { Request, Response } from "express";
+import express from "express";
 
 const app = express();
 const PORT = 8080; 
@@ -8,8 +7,16 @@ app.get("/healthz", (req, res) => {
     res.set({
             "Content-type": "text/plain",
             "charset": "utf-8"
-        })
-    res.send("OK")
+        });
+    res.send("OK");
+});
+
+app.get("/", (req, res) => {
+    res.set({
+            "Content-type": "text/plain",
+            "charset": "utf-8"
+        });
+    res.redirect(302, "/app/");
 });
 
 app.use("/app", express.static("./src/app"));
@@ -17,8 +24,3 @@ app.use("/app", express.static("./src/app"));
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
 });
-
-async function handlerReadiness(req: Request, res: Response): Promise<void> {
-    res.set
-    res.send()
-}
