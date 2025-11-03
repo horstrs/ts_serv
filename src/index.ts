f49a6dfba5e5fd21bf173b6a8f64f4ad8ad043a8
horstrs <horst.marcelo@gmail.com>
2025-11-03 15:57:30 -0300

    error handling

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 
 import { handlerMetrics, handlerReadiness, handlerRerouteHome, handlerReset } from "./api/handlers.js";
 import { hanlderValidate } from "./api/validate_chirp.js"
+import { errorHandler } from "./api/errorHandler.js";
 import { middlewareLogResponse, middlewareMetricsInc } from "./api/middleware.js";
 
 
@@ -12,12 +13,27 @@ app.use(express.json());
 app.use(middlewareLogResponse);
 app.use("/app", middlewareMetricsInc, express.static("./src/app"));
 
-app.get("/api/healthz", handlerReadiness);
-app.get("/admin/metrics", handlerMetrics);
-app.get("/", handlerRerouteHome);
+app.get("/api/healthz", (req, res, next) => {
+  Promise.resolve(handlerReadiness(req,res)).catch(next);
+});
 
-app.post("/admin/reset", handlerReset);
-app.post("/api/validate_chirp", hanlderValidate);
+app.get("/admin/metrics", (req, res, next) => {
+   Promise.resolve(handlerMetrics(req,res)).catch(next);
+  });
+
+app.get("/", (req, res, next) => {
+  Promise.resolve(handlerRerouteHome(req,res)).catch(next);
+});
+
+app.post("/admin/reset", (req, res, next) => {
+  Promise.resolve(handlerReset(req,res)).catch(next);
+});
+
+app.post("/api/validate_chirp", (req, res, next) => {
+  Promise.resolve(hanlderValidate(req,res)).catch(next);
+});
+
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
