c635adec33132015501d32825aec01e58040d1dd
horstrs <horst.marcelo@gmail.com>
2025-11-03 12:59:24 -0300

    Split API paths

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,19 @@
 import express from "express";
 
-import { handlerReadiness } from "./api/readiness.js";
-import { middlewareLogResponse } from "./api/middleware.js";
+import { handlerMetrics, handlerReadiness, handlerRerouteHome, handlerReset } from "./api/handlers.js";
+import { middlewareLogResponse, middlewareMetricsInc } from "./api/middleware.js";
+
 
 const app = express();
 const PORT = 8080;
 
 app.use(middlewareLogResponse);
-app.use("/app", express.static("./src/app"));
+app.use("/app", middlewareMetricsInc, express.static("./src/app"));
 
-app.get("/healthz", handlerReadiness);
-
-app.get("/", (req, res) => {
-    res.set({
-            "Content-type": "text/plain",
-            "charset": "utf-8"
-        });
-    res.redirect(302, "/app/");
-});
+app.get("/api/healthz", handlerReadiness);
+app.get("/api/metrics", handlerMetrics);
+app.get("/api/reset", handlerReset);
+app.get("/", handlerRerouteHome);
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
