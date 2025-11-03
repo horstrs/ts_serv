744be59f767ae389c1b5202452da80c0d4321a27
horstrs <horst.marcelo@gmail.com>
2025-11-03 15:02:18 -0300

    Added message validation

--- src/api/validate_chirp.ts
+++ src/api/validate_chirp.ts
@@ -13,5 +13,19 @@ export async function hanlderValidate(req: Request, res: Response) {
     respondWithError(res, 400, "Chirp is too long");
     return;
   }
-  respondWithJSON(res, 200, {valid: true});
+  const cleanedBody = cleanMessages(parsedBody.body)
+  respondWithJSON(res, 200, {cleanedBody: cleanedBody});
+}
+
+function cleanMessages(message: String): String {
+  const improperWords = ["kerfuffle", "sharbert", "fornax"];
+  let cleanedMessage = message;
+  const inputArray = message.split(" ");
+  for (const word of inputArray){
+    if (improperWords.includes(word.toLowerCase())) {
+      const split = cleanedMessage.split(word);
+      cleanedMessage = split.join("****");
+    }
+  }
+  return cleanedMessage
 }
